c4823863673c59c5adebef59621d19df96a46891
mechmind-dwv <mechmind-dwv@github.com>
2025-06-02 00:05:45 +0200

    🔄 Hard reset structure

--- projects/proyecto_rust/src/main.rs
+++
@@ -1,8 +0,0 @@
-<div align="center">
-  <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDl6d3V0b2N0Z3V4bHl5dWZ4Z2J6eGJmN2RycG5xZ2NqYzB1eWZ1ZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oKIPnAiaMCws8nOsE/giphy.gif" width="250">
-
-  ![Rust](https://img.shields.io/badge/Rust-000000?style=for-the-badge&logo=rust&logoColor=white)
-  ![ROS2](https://img.shields.io/badge/ROS2-22314E?style=for-the-badge&logo=ros&logoColor=white)
-  ![Embedded](https://img.shields.io/badge/Embedded-00599C?style=for-the-badge&logo=embeddedc&logoColor=white)
-  ![Crates.io](https://img.shields.io/crates/v/serde?style=for-the-badge&logo=rust)
-</div>
