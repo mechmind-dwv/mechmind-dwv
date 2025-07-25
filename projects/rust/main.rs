83e430e7bc62c3b1509f42d2f260e6d14da1b31f
IA. MechMind <ia.mechmind@gmail.com>
2025-07-25 17:01:50 +0200

    Update main.rs (#81)
    Signed-off-by: IA. MechMind <ia.mechmind@gmail.com>

--- projects/rust/main.rs
+++ projects/rust/main.rs
@@ -10,3 +10,8 @@ fn test_codeql_setup() {
     assert!(std::path::exists(".github/codeql"));
     println!("✅ Todo listo para escanear");
 }
+// ❌ Inseguro
+unsafe { std::ptr::read(0x0 as *const u32) };
+
+// ✅ Seguro
+let value = some_safe_api.get_data()
