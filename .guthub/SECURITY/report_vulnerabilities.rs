ea959fb9f6a5b949da6a0c2da913a3ceab8ba076
IA. MechMind <ia.mechmind@gmail.com>
2025-07-26 11:09:33 +0200

    Create report_vulnerabilities.rs
    Signed-off-by: IA. MechMind <ia.mechmind@gmail.com>

---
+++ .guthub/SECURITY/report_vulnerabilities.rs
@@ -0,0 +1,10 @@
+## 🚨 Reportar Vulnerabilidades
+**Áreas críticas de atención:**
+- `unsafe` blocks en código Rust
+- Configuraciones ROS2 expuestas
+- Credenciales hardcodeadas
+
+**Canales seguros:**
+- 📧 Email: [security@mechmind.tech](mailto:security@mechmind.tech) (PGP: `0xDEADBEEF`)
+- 🔐 [Formulario seguro](https://github.com/mechmind-dwv/security) (requiere autenticación GitHub)
+- 🤖 Issue con label `[SECURITY]`
