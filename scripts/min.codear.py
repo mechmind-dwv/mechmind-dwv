4987097021394d46b115f2f42e9a600a7c129c17
mechmin-dwv <ia.mechmind@gmail.com>
2026-08-04 19:20:33 +0200

    fix: repair min.codear.py (define Min class) and CodeQL config (use codeql/ scope)

--- scripts/min.codear.py
+++ scripts/min.codear.py
@@ -1,7 +1,31 @@
-while True:
-    min.codear()
-    min.tomar_café()
-    if "bug" in min.código:
-        min.debuggear(con_frustración=True)
-    # Añadimos una salida para evitar bucle infinito en análisis estático
-    break
+# -*- coding: utf-8 -*-
+"""
+Script de humor sobre programación.
+Define un objeto 'min' con métodos para codear, tomar café y debuggear.
+"""
+
+class Min:
+    """Representa al programador mínimo."""
+    
+    def codear(self):
+        print("💻 Codificando...")
+    
+    def tomar_café(self):
+        print("☕ Tomando café...")
+    
+    def debuggear(self, con_frustración=True):
+        if con_frustración:
+            print("🐛 ¡Maldito bug! (╯°□°)╯︵ ┻━┻")
+        else:
+            print("🐛 Debuggeando con calma...")
+
+# Instancia global
+min = Min()
+
+if __name__ == "__main__":
+    while True:
+        min.codear()
+        min.tomar_café()
+        if "bug" in "el código siempre tiene bugs":
+            min.debuggear(con_frustración=True)
+        break  # Para evitar bucle infinito al ejecutarlo
